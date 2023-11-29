987432700742d18ad386324ba524ee7fb9b905bd
Brooke Elliott <brookeelliott711@gmail.com>
2023-11-28 22:57:21 -0700

    linq exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var MovieNames = new List<string>() { "Die Hard", "James Bond", "Hot Fuzz", "Harry Potter" };
+
+            var SortedMovieNames = MovieNames.OrderBy(size => size.Length).ToList();
+
+            foreach (var i in SortedMovieNames)
+            {
+                Console.WriteLine($"{i}");
+            }
         }
     }
 }
